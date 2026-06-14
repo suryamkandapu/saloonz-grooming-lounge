98dde5b140411d91adc6cd57392eb2a898b0ebcd
Suryam205 <suryamkandapu205@gmail.com>
2026-06-14 20:04:49 +0530

    Tailwind issues set

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tsconfigPaths({ root: "." })],
+  plugins: [
+    tsconfigPaths({ root: "." }),
+    tailwindcss(),
+    tanstackStart(),
+    react(),
+  ],
   server: {
     headers: {
       "Cache-Control": "public, max-age=0",
